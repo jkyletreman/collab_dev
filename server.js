f5df4b32cd4d07004527e46d39e70fe801f93ad3
Kyle Treman <jkt290@nyu.edu>
2018-03-15 12:01:07 -0400

    Add routes
    Add routes for about, contact and nodemon

--- server.js
+++ server.js
@@ -7,4 +7,12 @@ app.use('/', (req, res) => {
   res.send('Hi');
 });
 
+app.use('/about', (req, res) => {
+  res.send("About");
+});
+
+app.use('/contact', (req, res) => {
+  res.send("Contact");
+});
+
 app.listen(PORT);
