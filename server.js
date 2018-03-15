8e14ed8c047efd3ce06051ac757e1888b1f31b80
Kyle Treman <jkt290@nyu.edu>
2018-03-15 16:48:56 -0400

    Staging for pull

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
+
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use('/about', (req, res) => {
   res.send('About');
