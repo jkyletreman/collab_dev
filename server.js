0825fe2ec11f214b05721c16acac5289268a1679
Kyle Treman <jkt290@nyu.edu>
2018-03-15 12:55:07 -0400

    Restarted

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const PORT = process.env.PORT || 8000;
 
 app.set('view engine', 'ejs')
 
-app.use('/', (req, res) => {
-  res.send('Hi');
-});
-
 app.use('/about', (req, res) => {
   res.send("About");
 });
 
 app.use('/contact', (req, res) => {
   res.send("Contact");
+});
+
+app.use('/', (req, res) => {
+  res.send('Hi');
 });
 
 app.listen(PORT);
