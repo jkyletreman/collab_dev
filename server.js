0b972b9c9d4999d02a9ae81a9a449233111d70ba
Kyle Treman <jkt290@nyu.edu>
2018-03-15 11:25:24 -0400

    Add eslint

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use('/', (req, res) => {
-  res.send("Hi");
+  res.send('Hi');
 });
 
-app.listen(PORT, console.log('Ready to serve....'));
+app.listen(PORT);
