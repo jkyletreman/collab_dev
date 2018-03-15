6f00de3195ce54572ba27032d2d401b0f22777f6
Kyle Treman <jkt290@nyu.edu>
2018-03-15 12:57:28 -0400

    Fix linting

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use('/about', (req, res) => {
-  res.send("About");
+  res.send('About');
 });
 
 app.use('/contact', (req, res) => {
-  res.send("Contact");
+  res.send('Contact');
 });
 
 app.use('/', (req, res) => {
